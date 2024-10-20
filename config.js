85bf66de3347b12c3a9642cee2307897b176f7b6
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-10-20 17:19:51 +0530

    ten

--- config.js
+++ config.js
@@ -3,4 +3,11 @@ require('dotenv').config() // Load environment variables from .env file
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy
 const Channel = require("@models/Channel") // Import the Channel model
-const { createUniqueHandle } = require("@lib/utils")
+const { createUniqueHandle } = require("@lib/utils");
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID, // Google client ID from environment variable
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Google client secret from environment variable
+            callbackURL: "/api/auth/google/callback", // Callback URL after Google authentication
+        },
