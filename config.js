c6d056728c031004895d4d47071bc8d777e26d87
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-10-21 20:55:27 +0530

    elven

--- config.js
+++ config.js
@@ -11,3 +11,7 @@ passport.use(
             clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Google client secret from environment variable
             callbackURL: "/api/auth/google/callback", // Callback URL after Google authentication
         },
+        async (accessToken, refreshToken, profile, cb) => {
+            try {
+                // Find a channel by email
+                let channel = await Channel.findOne({ email: profile.emails[0].value })
